2b369d0691316735508a1589d0f125694e3a3c3c
Udit Singh <udit@pop-os.localdomain>
2021-07-06 12:47:04 +0530

    Updates

---
+++ precache-manifest.fac91f295a340d92a66ef594181dd41c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9a3767318d156107615296b9202565",
+    "url": "/udit-portfolio/index.html"
+  },
+  {
+    "revision": "fd8833519b30b1868542",
+    "url": "/udit-portfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "6280ac6a11458ee42b03",
+    "url": "/udit-portfolio/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "158a5901dd625c8d8be9",
+    "url": "/udit-portfolio/static/css/main.65554146.chunk.css"
+  },
+  {
+    "revision": "1af5b293325efc7488a4",
+    "url": "/udit-portfolio/static/js/2.ab4777c0.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/udit-portfolio/static/js/2.ab4777c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8833519b30b1868542",
+    "url": "/udit-portfolio/static/js/3.f93dbc3a.chunk.js"
+  },
+  {
+    "revision": "6280ac6a11458ee42b03",
+    "url": "/udit-portfolio/static/js/4.b8e04de1.chunk.js"
+  },
+  {
+    "revision": "158a5901dd625c8d8be9",
+    "url": "/udit-portfolio/static/js/main.1c24dee8.chunk.js"
+  },
+  {
+    "revision": "88fce8adb8b951cd2794",
+    "url": "/udit-portfolio/static/js/runtime-main.e7d840cb.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/udit-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/udit-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/udit-portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/udit-portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/udit-portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/udit-portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "84273e76a5c9dfc61fd6e875c42ab0cd",
+    "url": "/udit-portfolio/static/media/grroom_logo.84273e76.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/udit-portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/udit-portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/udit-portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/udit-portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/udit-portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/udit-portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
