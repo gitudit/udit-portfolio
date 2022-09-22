b2b11ea76342a14b66548e775afde9284b5ddd22
gitudit <rafanadal.94513@gmail.com>
2022-09-22 18:37:54 +0530

    updated resume

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -79,7 +79,7 @@ function Header() {
             </li>
           )}
           <li>
-            <a href="https://drive.google.com/file/d/1a6_INy0hzyYDdKNyiikZQrkamzb2sKZv/view?usp=sharing" target="_blank" rel="noreferrer noopener">Resume</a>
+            <a href="https://drive.google.com/file/d/19H6_bZmK0ERZkOgSh3GjIFnmfphFJIcL/view?usp=sharing" target="_blank" rel="noreferrer noopener">Resume</a>
             {/* <a href="#" target="_blank" rel="noreferrer noopener">Resume</a> */}
 
           </li>
