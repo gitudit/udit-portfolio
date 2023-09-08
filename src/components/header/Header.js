52f81ec5bce997dd4342325bd53c5561c3c5b9da
gitudit <87002353+gitudit@users.noreply.github.com>
2023-09-08 16:34:39 +0530

    Update Resume

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -79,7 +79,7 @@ function Header() {
             </li>
           )}
           <li>
-            <a href="https://drive.google.com/file/d/175B9GWzSx2MdYQ7HW8yhV9KI_QM1TOTj/view?usp=sharing" target="_blank" rel="noreferrer noopener">Resume</a>
+            <a href="https://drive.google.com/file/d/1PfVK6vOriO007ibnTvPmHViFnjrt8SDG/view?usp=sharing" target="_blank" rel="noreferrer noopener">Resume</a>
             {/* <a href="#" target="_blank" rel="noreferrer noopener">Resume</a> */}
 
           </li>
